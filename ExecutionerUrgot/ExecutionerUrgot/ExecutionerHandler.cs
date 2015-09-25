6ec213fef90eb2f87040ceab7da101bcc4f1f3c3
CounterFX <counterfx.central@gmail.com>
2015-09-24 22:45:26 -0400

    Creating Code

---
+++ ExecutionerUrgot/ExecutionerUrgot/ExecutionerHandler.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace ExecutionerUrgot
+{
+    class ExecutionerHandler
+    {
+        /* Grab Spell */
+        public enum AttackSpell
+        {
+            Q,
+            W,
+            E,
+            I,
+        }
+
+        /* Grab Player */
+        public static AIHeroClient Urgot { get { return ObjectManager.Player; } }
+
+        /* Grab Enemies */
+        public static Obj_AI_Base GetEnemy(float range, GameObjectType gametype)
+        {
+            return ObjectManager.Get<Obj_AI_Base>().OrderBy(a => a.Health)
+                    .Where(a => a.IsEnemy
+                    && a.Type == gametype && !Urgot.IsRecalling
+                    && !a.IsDead && a.IsValidTarget(range) && !a.IsInvulnerable
+                    && a.Distance(Urgot) <= range).FirstOrDefault();
+        }
+        public static Obj_AI_Base GetEnemyKS(AttackSpell spell, GameObjectType gametype)
+        {
+            /* Last hit with Q */
+            if (spell == AttackSpell.Q)
+            {
+                return ObjectManager.Get<Obj_AI_Base>().OrderBy(a => a.Health)
+                    .Where(a => a.IsEnemy
+                    && a.Type == gametype
+                    && !a.IsDead && a.IsValidTarget(Program.Q.Range) && !a.IsInvulnerable
+                    && a.Health <= QDamage(a)
+                    && a.Distance(Urgot) <= Program.Q.Range).FirstOrDefault();
+            }
+
+            /* Last hit with W */
+            else if (spell == AttackSpell.W)
+            {
+                return ObjectManager.Get<Obj_AI_Base>()
+                    .Where(a => a.IsEnemy
+                    && a.Type == gametype
+                    && !a.IsDead && a.IsValidTarget(Program.W.Range) && !a.IsInvulnerable
+                    && a.Health <= WDamage(a)
+                    && a.Distance(Urgot) <= Program.W.Range).FirstOrDefault();
+            }
+
+            /* Last hit with E */
+            else if (spell == AttackSpell.E)
+            {
+                return ObjectManager.Get<Obj_AI_Base>()
+                    .Where(a => a.IsEnemy
+                    && a.Type == gametype
+                    && !a.IsDead && a.IsValidTarget(Program.E.Range) && !a.IsInvulnerable
+                    && a.Health <= EDamage(a)
+                    && a.Distance(Urgot) <= Program.E.Range).FirstOrDefault();
+            }
+            else if (spell == AttackSpell.I)
+            {
+                return ObjectManager.Get<Obj_AI_Base>()
+                    .Where(a => a.IsEnemy
+                    && a.Type == gametype
+                    && !a.IsDead && a.IsValidTarget(Program.Ignite.Range) && !a.IsInvulnerable
+                    && a.Health <= Urgot.GetSummonerSpellDamage(a, DamageLibrary.SummonerSpells.Ignite)
+                    && a.Distance(Urgot) <= Program.Ignite.Range).FirstOrDefault();
+            }
+            else
+                return null;
+        }
+
+        /* Grab Items */
+        public static InventorySlot[] UrgotItems { get { return Urgot.InventoryItems; } }
+
+        /* Damage Calculator */
+        static float QDamage(Obj_AI_Base target)
+        {
+            return Urgot.CalculateDamageOnUnit(target, DamageType.Magical,
+                new float[] { 0, 60, 85, 110, 135, 160 }[Program.Q.Level]
+                + (0.55f * Urgot.FlatMagicDamageMod)
+                + (new float[] { 0f, 0.02f, 0.025f, 0.03f, 0.035f, 0.04f }[Program.Q.Level] * Urgot.MaxMana));
+        }
+        static float WDamage(Obj_AI_Base target)
+        {
+            return Urgot.CalculateDamageOnUnit(target, DamageType.Magical,
+                new float[] { 0, 80, 100, 120, 140, 160 }[Program.W.Level]
+                + (0.4f * Urgot.FlatMagicDamageMod)
+                + (0.025f * Urgot.MaxMana));
+        }
+        static float EDamage(Obj_AI_Base target)
+        {
+            return Urgot.CalculateDamageOnUnit(target, DamageType.Magical,
+                new float[] { 0, 36, 52, 68, 84, 100 }[Program.E.Level]
+                + (0.2f * Urgot.FlatMagicDamageMod)
+                + (0.02f * Urgot.MaxMana));
+        }
+
+        /* Features Handler */
+        public static void ComboMode()
+        {
+            /* Q in combo mode */
+            if (Program.ComboMenu["Qcombo"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qcombo = GetEnemy(Program.Q.Range, GameObjectType.AIHeroClient);
+                if (Qcombo != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qcombo);
+                }
+            }
+
+            /* W in combo mode */
+            if (Program.ComboMenu["Wcombo"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Wcombo = GetEnemy(Program.W.Range, GameObjectType.AIHeroClient);
+                if (Wcombo != null)
+                {
+                    if (Program.W.IsReady())
+                    {
+                        Program.W.Cast(Wcombo);
+                    }
+                }
+            }
+
+            /* E in combo mode */
+            if (Program.ComboMenu["Ecombo"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Ecombo = GetEnemy(Program.E.Range, GameObjectType.AIHeroClient);
+                if (Ecombo != null)
+                {
+                    if (Program.E.IsReady())
+                        Program.E.Cast(Ecombo);
+                }
+            }
+
+            /* R in combo mode with root */
+            if (Program.ComboMenu["Rcombo"].Cast<CheckBox>().CurrentValue == true)
+            {
+                AIHeroClient Rcombo = ObjectManager.Get<AIHeroClient>().Where(a => a.IsEnemy
+                 && a.Distance(Urgot) <= Program.W.Range
+                && a.HasBuff("UrgotW")).FirstOrDefault();
+                if (Rcombo != null)
+                {
+                    if (Program.R.IsReady())
+                        Program.R.Cast();
+                }
+            }
+
+            /* R in combo mode without root */
+            if (Program.ComboMenu["Rcombo"].Cast<CheckBox>().CurrentValue == false)
+            {
+                AIHeroClient Rcombo = ObjectManager.Get<AIHeroClient>().Where(a => a.IsEnemy && a.Distance(Urgot) <= Program.Q.Range).FirstOrDefault();
+                if (Rcombo != null)
+                {
+                    if (Program.R.IsReady())
+                        Program.R.Cast();
+                }
+            }
+        }
+        public static void HarassMode()
+        {
+            /* Q in harass mode */
+            if (Program.HarassMenu["Qharass"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qharass = GetEnemy(Program.Q.Range, GameObjectType.AIHeroClient);
+                if (Qharass != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qharass);
+                }
+            }
+        }
+        public static void JungleMode()
+        {
+            /* Q in jungle mode */
+            if (Program.JungleMenu["Qjungle"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qcamp = GetEnemy(Program.Q.Range, GameObjectType.obj_AI_Minion);
+                if (Qcamp != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qcamp);
+                }
+            }
+
+            /* W in jungle mode */
+            if (Program.JungleMenu["Wjungle"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Wcamp = GetEnemy(Program.W.Range, GameObjectType.obj_AI_Minion);
+                if (Wcamp != null)
+                {
+                    if (Program.W.IsReady())
+                        Program.W.Cast(Wcamp);
+                }
+            }
+
+            /* E in jungle mode */
+            if (Program.JungleMenu["Ejungle"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Ecamp = GetEnemy(Program.E.Range, GameObjectType.obj_AI_Minion);
+                if (Ecamp != null)
+                {
+                    if (Program.E.IsReady())
+                        Program.E.Cast(Ecamp);
+                }
+            }
+        }
+        public static void LaneClearMode()
+        {
+            if (Program.LaneClearMenu["Qlanec"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qminion = GetEnemy(Program.Q.Range, GameObjectType.obj_AI_Minion);
+                if (Qminion != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qminion);
+                }
+            }
+
+            if (Program.LaneClearMenu["Wlanec"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Wminion = GetEnemy(Program.W.Range, GameObjectType.obj_AI_Minion);
+                if (Wminion != null)
+                {
+                    if (Program.W.IsReady())
+                        Program.W.Cast(Wminion);
+                }
+            }
+
+            if (Program.LaneClearMenu["Wlanec"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Eminion = GetEnemy(Program.E.Range, GameObjectType.obj_AI_Minion);
+                if (Eminion != null)
+                {
+                    if (Program.E.IsReady())
+                        Program.E.Cast(Eminion);
+                }
+            }
+        }
+        public static void LastHitMode()
+        {
+            if (Program.LastHitMenu["Qlasthit"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qminion = GetEnemyKS(AttackSpell.Q, GameObjectType.obj_AI_Minion);
+                if (Qminion != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qminion);
+                }
+            }
+
+            if (Program.LastHitMenu["Wlasthit"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Wminion = GetEnemyKS(AttackSpell.W, GameObjectType.obj_AI_Minion);
+                if (Wminion != null)
+                {
+                    if (Program.W.IsReady())
+                        Program.W.Cast(Wminion);
+                }
+            }
+        }
+        public static void KSMode()
+        {
+            if (Program.SettingMenu["KSmode"].Cast<CheckBox>().CurrentValue)
+            {
+                Obj_AI_Base Qks = GetEnemyKS(AttackSpell.Q, GameObjectType.AIHeroClient);
+                if (Qks != null)
+                {
+                    if (Program.Q.IsReady())
+                        Program.Q.Cast(Qks);
+                }
+
+                Obj_AI_Base Wks = GetEnemyKS(AttackSpell.W, GameObjectType.AIHeroClient);
+                if (Wks != null)
+                {
+                    if (Program.W.IsReady())
+                        Program.W.Cast(Wks);
+                }
+
+                Obj_AI_Base Eks = GetEnemyKS(AttackSpell.E, GameObjectType.AIHeroClient);
+                if (Eks != null)
+                {
+                    if (Program.E.IsReady())
+                        Program.E.Cast(Eks);
+                }
+            }
+        }
+        public static void IgniteMode()
+        {
+            Obj_AI_Base target = GetEnemyKS(AttackSpell.I, GameObjectType.AIHeroClient);
+            if (target != null)
+            {
+                if (Program.Ignite.IsReady())
+                    Program.Ignite.Cast(target);
+            }
+        }
+
+        /* Misc Modes */
+        public static void DrawMode()
+        {
+            Drawing.DrawCircle(Urgot.Position, Program.Q.Range, Color.LawnGreen);
+            Drawing.DrawCircle(Urgot.Position, 1200, Color.OrangeRed);
+            Drawing.DrawCircle(Urgot.Position, Program.E.Range, Color.SpringGreen);
+            Drawing.DrawCircle(Urgot.Position, Program.R.Range, Color.DarkGreen);
+        }
+        public static void StackMode()
+        {
+            foreach (InventorySlot item in UrgotItems)
+            {
+                if ((item.Id == ItemId.Tear_of_the_Goddess || item.Id == ItemId.Tear_of_the_Goddess_Crystal_Scar
+                        || item.Id == ItemId.Archangels_Staff || item.Id == ItemId.Archangels_Staff_Crystal_Scar
+                        || item.Id == ItemId.Manamune || item.Id == ItemId.Manamune_Crystal_Scar)
+                    && item.Stacks < 750
+                    && Urgot.IsInShopRange()
+                    && Program.Q.IsReady())
+                {
+                    Program.Q.Cast(Urgot.Position);
+                }
+            }
+        }
+        public static void HealthPotionMode()
+        {
+            foreach (InventorySlot item in UrgotItems)
+            {
+                if (item.Id == ItemId.Health_Potion
+                    && Urgot.Health <= (Urgot.MaxHealth * (0.01 * Program.SettingMenu["Healthcall"].Cast<Slider>().CurrentValue))
+                    && !Urgot.IsRecalling()
+                    && !Urgot.IsInShopRange()
+                    && !Urgot.HasBuff("RegenerationPotion"))
+                {
+                    item.Cast();
+                }
+            }
+        }
+        public static void ManaPotionMode()
+        {
+            foreach (InventorySlot item in UrgotItems)
+            {
+                if (item.Id == ItemId.Mana_Potion
+                    && Urgot.Mana <= (Urgot.MaxMana * (0.01 * Program.SettingMenu["FlaskHcall"].Cast<Slider>().CurrentValue))
+                    && !Urgot.IsRecalling()
+                    && !Urgot.IsInShopRange()
+                    && !Urgot.HasBuff("FlaskOfCrystalWater"))
+                {
+                    item.Cast();
+                }
+            }
+        }
+        public static void CrystallineFlaskMode()
+        {
+            foreach (InventorySlot item in UrgotItems)
+            {
+                /* Flask Health Call */
+                if (item.Id == ItemId.Crystalline_Flask
+                    && Urgot.Health <= (Urgot.MaxHealth * (0.01 * Program.SettingMenu["FlaskHcall"].Cast<Slider>().CurrentValue))
+                    && !Urgot.IsRecalling()
+                    && !Urgot.IsInShopRange()
+                    && !Urgot.HasBuff("ItemCrystalFlask"))
+                {
+                    item.Cast();
+                }
+                /* Flask Mana Call */
+                if (item.Id == ItemId.Crystalline_Flask
+                    && Urgot.Mana <= (Urgot.MaxMana * (0.01 * Program.SettingMenu["FlaskMcall"].Cast<Slider>().CurrentValue))
+                    && !Urgot.IsRecalling()
+                    && !Urgot.IsInShopRange()
+                    && !Urgot.HasBuff("ItemCrystalFlask"))
+                {
+                    item.Cast();
+                }
+            }
+        }
+        public static void LevelerMode(Obj_AI_Base sender, Obj_AI_BaseLevelUpEventArgs args)
+        {
+            if (!sender.IsMe)
+            {
+                return;
+            }
+
+            int[] array = new int[] { 0, 1, 3, 1, 2, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+            int skill = array[args.Level];
+
+            if (skill == 1)
+                Urgot.Spellbook.LevelSpell(SpellSlot.Q);
+            else if (skill == 2)
+                Urgot.Spellbook.LevelSpell(SpellSlot.W);
+            else if (skill == 3)
+                Urgot.Spellbook.LevelSpell(SpellSlot.E);
+            else if (skill == 4)
+                Urgot.Spellbook.LevelSpell(SpellSlot.R);
+            else
+                return;
+        }
+    }
+}
--- ExecutionerUrgot/ExecutionerUrgot/Program.cs
+++ ExecutionerUrgot/ExecutionerUrgot/Program.cs
@@ -1,15 +1,196 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 
 namespace ExecutionerUrgot
 {
+    /* Created by Counter */
+
+    /* To Do:
+        
+        * Ignite + Smite input
+        * don't Q Wind Wall/Unbreakable/BlackShield
+         
+    */
     class Program
     {
+        /* Menus */
+        public static Menu ExecutionerUrgotMenu, SettingMenu, ComboMenu, HarassMenu, JungleMenu, LaneClearMenu, LastHitMenu;
+        // Skills
+        public static Spell.Skillshot Q;
+        public static Spell.Active Q2;
+        public static Spell.Active W;
+        public static Spell.Skillshot E;
+        public static Spell.Targeted R;
+        public static Spell.Targeted Ignite;
+
         static void Main(string[] args)
         {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
         }
+
+        static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            /* Confirming Champion */
+            if (Player.Instance.ChampionName != "Urgot")
+            {
+                return;
+            }
+
+            Bootstrap.Init(null);
+
+            Q = new Spell.Skillshot(SpellSlot.Q, 1000, SkillShotType.Linear);
+            Q2 = new Spell.Active(SpellSlot.Q);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Skillshot(SpellSlot.E, 900, SkillShotType.Circular);
+            R = new Spell.Targeted(SpellSlot.R, 550);
+            if (Player.GetSpell(SpellSlot.Summoner1).Name == "Summonerdot")
+            {
+                Ignite = new Spell.Targeted(SpellSlot.Summoner1, 600);
+            }
+            if (Player.GetSpell(SpellSlot.Summoner2).Name == "Summonerdot")
+            {
+                Ignite = new Spell.Targeted(SpellSlot.Summoner2, 600);
+            }
+
+            ExecutionerUrgotMenu = MainMenu.AddMenu("Executioner Urgot", "ExecutionerUrgot");
+            ExecutionerUrgotMenu.AddGroupLabel("Executioner Urgot");
+
+            SettingMenu = ExecutionerUrgotMenu.AddSubMenu("Settings", "Settings");
+            SettingMenu.AddGroupLabel("Settings");
+            SettingMenu.AddSeparator();
+            SettingMenu.Add("Drawmode", new CheckBox("Drawing Mode"));
+            SettingMenu.Add("KSmode", new CheckBox("KS Mode"));
+            SettingMenu.Add("Stackmode", new CheckBox("Stack Tear Mode"));
+            SettingMenu.Add("Ignitemode", new CheckBox("Auto Ignite"));
+            SettingMenu.AddSeparator();
+            SettingMenu.AddLabel("Health Potion/Mana Potion/Crystalline Flask Activator - 0 is off");
+            SettingMenu.Add("Healthcall", new Slider("Use Health Potion if Health %", 25, 0, 100));
+            SettingMenu.Add("Manacall", new Slider("Use Mana Potion if Mana %", 25, 0, 100));
+            SettingMenu.Add("FlaskHcall", new Slider("Use Crystalline Flask if Health %", 25, 0, 100));
+            SettingMenu.Add("FlaskMcall", new Slider("Use Crystalline Flask if Mana %", 25, 0, 100));
+
+            ComboMenu = ExecutionerUrgotMenu.AddSubMenu("Combo Features", "ComboFeatures");
+            ComboMenu.AddGroupLabel("Combo Features");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("Qcombo", new CheckBox("Q"));
+            ComboMenu.Add("Wcombo", new CheckBox("W"));
+            ComboMenu.Add("Ecombo", new CheckBox("E"));
+            ComboMenu.Add("Rcombo", new CheckBox("Use R with rooting"));
+
+            HarassMenu = ExecutionerUrgotMenu.AddSubMenu("Harass Features", "HarassFeatures");
+            HarassMenu.AddGroupLabel("Harass Features");
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("Qharass", new CheckBox("Q"));
+
+            JungleMenu = ExecutionerUrgotMenu.AddSubMenu("Jungle Features", "JungleFeatures");
+            JungleMenu.AddGroupLabel("Jungle Features");
+            JungleMenu.AddSeparator();
+            JungleMenu.Add("Qjungle", new CheckBox("Q"));
+            JungleMenu.Add("Ejungle", new CheckBox("E"));
+
+            LaneClearMenu = ExecutionerUrgotMenu.AddSubMenu("Lane Clear Features", "LaneClearFeatures");
+            LaneClearMenu.AddGroupLabel("Lane Clear Features");
+            LaneClearMenu.AddSeparator();
+            LaneClearMenu.Add("Qlanec", new CheckBox("Q"));
+
+            LastHitMenu = ExecutionerUrgotMenu.AddSubMenu("Last Hit Features", "LastHitFeatures");
+            LastHitMenu.AddGroupLabel("Last Hit Features");
+            LastHitMenu.AddSeparator();
+            LastHitMenu.Add("Qlasthit", new CheckBox("Q"));
+
+            Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
+            Player.OnLevelUp += ExecutionerHandler.LevelerMode;
+            /*Gapcloser.OnGapCloser += Gapcloser_OnGapcloser;
+            Interrupter.OnInterruptableSpell += Interrupter_OnInterruptableSpell;*/
+
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            if (Program.SettingMenu["Drawmode"].Cast<CheckBox>().CurrentValue)
+            {
+                ExecutionerHandler.DrawMode();
+            }
+        }
+
+        static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                ExecutionerHandler.ComboMode();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                ExecutionerHandler.HarassMode();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                ExecutionerHandler.JungleMode();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                ExecutionerHandler.LaneClearMode();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
+            {
+                ExecutionerHandler.LastHitMode();
+            }
+            if (Program.SettingMenu["KSmode"].Cast<CheckBox>().CurrentValue)
+            {
+                ExecutionerHandler.KSMode();
+            }
+            if (Program.SettingMenu["Stackmode"].Cast<CheckBox>().CurrentValue)
+            {
+                ExecutionerHandler.StackMode();
+            }
+            if (Program.SettingMenu["Healthcall"].Cast<Slider>().CurrentValue > 0)
+            {
+                ExecutionerHandler.HealthPotionMode();
+            }
+            if (Program.SettingMenu["Manacall"].Cast<Slider>().CurrentValue > 0)
+            {
+                ExecutionerHandler.ManaPotionMode();
+            }
+            if (Program.SettingMenu["FlaskHcall"].Cast<Slider>().CurrentValue > 0 || Program.SettingMenu["FlaskMcall"].Cast<Slider>().CurrentValue > 0)
+            {
+                ExecutionerHandler.CrystallineFlaskMode();
+            }
+            if (Program.SettingMenu["Ignitemode"].Cast<CheckBox>().CurrentValue)
+            {
+                ExecutionerHandler.IgniteMode();
+            }
+
+            /* Menu Information */
+            if (ComboMenu["Rcombo"].Cast<CheckBox>().CurrentValue == false)
+            {
+                ComboMenu["Rcombo"].Cast<CheckBox>().DisplayName = "Use R without rooting";
+            }
+            if (ComboMenu["Rcombo"].Cast<CheckBox>().CurrentValue == true)
+            {
+                ComboMenu["Rcombo"].Cast<CheckBox>().DisplayName = "Use R with rooting";
+            }
+        }
+
+        /*static void Gapcloser_OnGapcloser(AIHeroClient target, GapCloserEventArgs args)
+        {
+            if (Program.W.IsReady())
+                Program.W.Cast(target);
+        }
+        static void Interrupter_OnInterruptableSpell(AIHeroClient target, Interrupter.OnInterruptableSpellDelegate args)
+        {
+            if (Program.W.IsReady())
+                Program.W.Cast(target);
+        }*/
     }
 }
