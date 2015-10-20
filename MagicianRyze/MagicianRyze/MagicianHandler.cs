68c644919219f395420babdd524bc04066591e8b
HeheheM <kefirowaty123@gmail.com>
2015-10-20 14:55:22 +0200

    Update MagicianHandler.cs

--- MagicianRyze/MagicianRyze/MagicianHandler.cs
+++ MagicianRyze/MagicianRyze/MagicianHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -34,7 +34,7 @@ namespace MagicianRyze
         {
             return ObjectManager.Get<Obj_AI_Base>().OrderBy(a => a.Health)
                     .Where(a => a.IsEnemy
-                    && a.Type == gametype && !Ryze.IsRecalling
+                    && a.Type == gametype && Ryze.IsRecalling
                     && !a.IsDead && a.IsValidTarget(range) && !a.IsInvulnerable
                     && a.Distance(Ryze) <= range).FirstOrDefault();
         }
